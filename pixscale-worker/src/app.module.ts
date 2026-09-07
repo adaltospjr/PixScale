342970f3e131f81ed68020f43133f4d8e9006d84
adaltospjr <daltospjr@gmail.com>
2026-09-07 20:19:25 -0300

    chore: test idempotency postgres

--- pixscale-worker/src/app.module.ts
+++ pixscale-worker/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { PaymentEventsController } from './infra/messaging/controllers/payment-events.controller';
-import { DatabaseModule } from './infra/database/database.module'; // 🌟 Importe o novo módulo de banco
+import { DatabaseModule } from './infra/database/database.module';
 import { CacheModule } from './infra/cache/cache.module';
+
 import { LiquidatePaymentUseCase } from './application/use-cases/liquidate-payment.use-case';
+
 import type { AccountRepository } from './domain/repository/account-repository.interface';
-import type { CacheService } from './domain/cache/cache-service.interface';
+
 import { HealthController } from './infra/health/health.controller';
 
 @Module({
@@ -14,16 +17,21 @@ import { HealthController } from './infra/health/health.controller';
       isGlobal: true,
       envFilePath: '../.env',
     }),
+
     DatabaseModule,
     CacheModule,
   ],
+
   controllers: [PaymentEventsController, HealthController],
+
   providers: [
     {
       provide: LiquidatePaymentUseCase,
-      useFactory: (accountRepository: AccountRepository, cacheService: CacheService) =>
-        new LiquidatePaymentUseCase(accountRepository, cacheService),
-      inject: ['ACCOUNT_REPOSITORY', 'CACHE_SERVICE'],
+
+      useFactory: (accountRepository: AccountRepository) =>
+        new LiquidatePaymentUseCase(accountRepository),
+
+      inject: ['ACCOUNT_REPOSITORY'],
     },
   ],
 })
--- pixscale-worker/src/domain/repository/account-repository.interface.ts
+++ pixscale-worker/src/domain/repository/account-repository.interface.ts
@@ -1,5 +1,17 @@
 export interface AccountRepository {
   ping(): Promise<void>;
-  findAccountByNumber(accountNumber: string): Promise<{ id: string; balance: number } | null>;
-  executeLiquidation(originAccount: string, destinationAccount: string, amount: number, idempotencyKey: string): Promise<boolean>;
+
+  findAccountByNumber(
+    accountNumber: string,
+  ): Promise<{ id: string; balance: number } | null>;
+
+  executeLiquidation(
+    originAccount: string,
+    destinationAccount: string,
+    amount: number,
+    idempotencyKey: string,
+  ): Promise<{
+    status: 'PROCESSED' | 'DUPLICATE';
+    transactionId: string;
+  }>;
 }
